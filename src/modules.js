b09360241f635eb3beab67f8ac12fa583a7bbd26
Светлана <autizm6kola@yandex.com>
2026-09-05 06:04:41 +0300

    чтение

--- src/modules.js
+++ src/modules.js
@@ -1,12 +1,13 @@
 export const modules = [
   
   // { id: 3, name: "Айболит", url: "https://autizm6kola-art.github.io/cht_aybolit" },
-  { id: 31, name: "Лис и пёс", url: "https://autizm6kola-art.github.io/lis_i_pes" },
+  // { id: 31, name: "Лис и пёс", url: "https://autizm6kola-art.github.io/lis_i_pes" },
   // { id: 9, name: "Груффало", url: "https://autizm6kola-art.github.io/cht_gruffalo" },
   // { id: 8, name: "Муха-цокотуха", url: "https://autizm6kola-art.github.io/mucha_g" },
   // { id: 12, name: "Дочурка Груф", url: "https://autizm6kola-art.github.io/gruffalo_doch" },
-  { id: 32, name: "Хоббит", url: "https://autizm6kola-art.github.io/hobbit2/" },
-  { id: 32, name: "Властелин колец", url: "https://autizm6kola-art.github.io/vlastelin" }
+  // { id: 32, name: "Хоббит", url: "https://autizm6kola-art.github.io/hobbit2/" },
+  { id: 33, name: "Властелин колец", url: "https://autizm6kola-art.github.io/vlastelin" },
+  { id: 34, name: "ЧТЕНИЕ", url: "https://autizm6kola-art.github.io/shkola_8_chtenie" }
 ];
 
 
